9bea6f5ba89b0c802abf41a8f729498c61536988
Michael Verdi <michael.v.verdi@gmail.com>
2016-01-12 09:13:17 -0500

    starting to style butter blog

--- config/initializers/butter.rb
+++ config/initializers/butter.rb
@@ -7,7 +7,7 @@ Buttercms.configure do |config|
   # Specify a custom layout for your blog so it matches the rest of your site.
   # For example uncomment the line below and define the layout in /view/layout/blog.html.erb
   # You can of course also use your existing main application layout.
-  # config.layout = "blog"
+  config.layout = "application"
 
   # Specify a custom parent controller for Butter's BlogController to inherit from.
   # By default Butter inherit's from ApplicationController but you can override this with your own
