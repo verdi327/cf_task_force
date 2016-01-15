26c7d5470d07fe3dd352b479bcf046bd848b148b
Michael Verdi <michael.v.verdi@gmail.com>
2016-01-15 17:08:26 -0500

    minor style tweaks, remove image validation on coaches

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -2,7 +2,6 @@ class Coach < ActiveRecord::Base
 	validates :name, presence: true
 	validates :title, presence: true
 	validates :description, presence: true
-	validates :images, presence: true
 	
 	mount_uploaders :images, ImageUploader
 end
