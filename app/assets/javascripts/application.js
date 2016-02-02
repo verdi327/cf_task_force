fc83882b87b1ccc55892d62c8362e08b417015c2
Michael Verdi <michael.v.verdi@gmail.com>
2016-02-02 11:38:44 -0500

    add GA tracking

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require ga_tracking.min
 //= require plugins/retina/retina.min 
 //= require jquery
 //= require jquery_ujs
