7a8bc5b9835397edc99d4a9b56c15fdd480ba627
Michael Verdi <michael.v.verdi@gmail.com>
2016-04-03 10:28:52 -0400

    remove grand opening banner, update grand opening page to show modal

--- app/assets/javascripts/grand_opening.js
+++ app/assets/javascripts/grand_opening.js
@@ -1,9 +1,3 @@
 $(".pages.grand_opening").ready(function() {
-	$("a.banner").hide();
-	$("#phone_number").mask("(999) 999-9999");
-
-	$("form[data-remote]").on("ajax:complete", function(e, data, status, xhr){
-		$("#phoneModal").modal("hide");
-		alert("All Set! We'll see you April 2nd");
-	})
+	$("#alertModal").modal("show");
 });
