7206fcfeaf8b87822f5eb817d126b1e036ae288c
Michael Verdi <michael.v.verdi@gmail.com>
2016-04-06 15:36:11 -0400

    trying to configure sidekiq settings

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,3 +4,7 @@ require 'sidekiq/web'
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   [user, password] == ["cigar city", "crossfit"]
 end
+
+Sidekiq.configure_client do |config|
+  config.redis = { :size => 1 }
+end
